eb1dd285aad07ec7dac18f3409ce2a5301541f1f
Mattias Åslund <mattias.aslund@shoppa.com>
2020-08-29 23:22:34 +0200

    Implemented "overwrite" which replaces addWithOverwrite

--- packages/tracked-redux/src/__tests__/_index.spec.ts
+++ packages/tracked-redux/src/__tests__/_index.spec.ts
@@ -1,11 +1,27 @@
-import {makeTracked} from '..'
+import { makeTracked, overwrite } from '..'
 
-test("makeTracked", ()=>{
+test("makeTracked", () => {
     const entity = {};
 
     const actual = makeTracked(entity)
-    
+
     expect(actual.current).toBe(entity)
     expect(actual.underlying).toBe(entity)
     expect(actual.loaded).toBe(entity)
+})
+
+
+test("overwrite replaces existing entity", () => {
+    const state = {
+        [1]: makeTracked({ id: 1, value: 'original' }),
+        [2]: makeTracked({ id: 2, value: 'original' }),
+        [3]: makeTracked({ id: 3, value: 'original' }),
+    }
+
+    const entity = { id: 2, value: 'changed' }
+    const expected = { ...state, [2]: makeTracked(entity) }
+
+    const actual = overwrite(state, entity, 'id')
+
+    expect(actual).toEqual(expected)
 })
--- packages/tracked-redux/src/index.ts
+++ packages/tracked-redux/src/index.ts
@@ -1,5 +1,17 @@
 import {Tracked} from "./types"
 
+type Dictionary<T> = {
+    [id: number]: T
+}
+
 export function makeTracked<T>(entity: T): Tracked<T> {
     return { loaded: entity, underlying: entity, current: entity };
+}
+
+export function overwrite<T>(state: Dictionary<Tracked<T>>, entity: T, idField: keyof(T)): Dictionary<Tracked<T>> {
+    const id = +entity[idField];
+    return {
+        ...state, 
+        [id]: makeTracked(entity)
+    }
 }
