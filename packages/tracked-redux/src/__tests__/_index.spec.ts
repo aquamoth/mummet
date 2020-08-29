93a26d59001151e068a698c9ecd233add4efd365
Mattias Åslund <mattias.aslund@shoppa.com>
2020-08-29 23:40:47 +0200

    deep-freezing test objects to make sure they are immutable.

--- packages/tracked-redux/src/__tests__/_index.spec.ts
+++ packages/tracked-redux/src/__tests__/_index.spec.ts
@@ -1,3 +1,4 @@
+import deepFreeze from "deep-freeze"
 import { makeTracked, overwrite } from '..'
 
 test("makeTracked", () => {
@@ -12,14 +13,28 @@ test("makeTracked", () => {
 
 
 test("overwrite replaces existing entity", () => {
-    const state = {
+    const state = deepFreeze({
         [1]: makeTracked({ id: 1, value: 'original' }),
         [2]: makeTracked({ id: 2, value: 'original' }),
         [3]: makeTracked({ id: 3, value: 'original' }),
-    }
-
-    const entity = { id: 2, value: 'changed' }
+    })
+    const entity = deepFreeze({ id: 2, value: 'changed' })
     const expected = { ...state, [2]: makeTracked(entity) }
+    
+    const actual = overwrite(state, entity, 'id')
+
+    expect(actual).toEqual(expected)
+})
+
+
+test("overwrite adds new entity", () => {
+    const state = deepFreeze({
+        [1]: makeTracked({ id: 1, value: 'original' }),
+        [2]: makeTracked({ id: 2, value: 'original' }),
+        [3]: makeTracked({ id: 3, value: 'original' }),
+    })
+    const entity = deepFreeze({ id: 4, value: 'changed' })
+    const expected = { ...state, [4]: makeTracked(entity) }
 
     const actual = overwrite(state, entity, 'id')
 
