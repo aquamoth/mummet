0f3661d9a00100392d9cdf24eb4b3c6f5397c69d
Mattias Åslund <mattias.aslund@shoppa.com>
2020-09-07 14:09:10 +0200

    Added missing export of track from helpers

--- packages/mummet-forms/src/index.ts
+++ packages/mummet-forms/src/index.ts
@@ -1,1 +1,28 @@
+import { addOrReplace } from 'mummet-core'
+import { Tracked, Dictionary } from 'mummet-core/dist/types';
+
 export default (text: string)=> "Hello " + text;
+
+
+type Projector = {}
+
+
+export const reducer = ()=> {
+    const x: Tracked<number> = track(4);
+
+
+    const isDirty = (x.current !== x.underlying)
+
+    
+    let  state: Dictionary<Tracked<Projector>, string> = undefined
+
+}
+
+
+
+
+
+
+function track<T>(entity: T): Tracked<T> {
+    return { loaded: entity, underlying: entity, current: entity };
+}
